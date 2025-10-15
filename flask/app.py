8ff6f632366d8d5fbc8f87fdc198f62c9355688d
LUIZ FERNANDO FRANZON <52421183@unimax.edu>
2025-10-15 20:31:46 -0300

    add: flask templates

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,11 @@
 from config import app, db
 from models import Aluno, Curso, Matricula
-from flask import request, jsonify
+from flask import request, jsonify, render_template, redirect, url_for
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 @app.route('/api/alunos', methods=['POST'])
@@ -31,6 +36,37 @@ def criar_matricula_api():
 def relatorio_api():
     resultado = db.session.query(Aluno.nome, Curso.nome).join(Matricula).join(Curso).all()
     return jsonify([{'aluno': r[0], 'curso': r[1]} for r in resultado])
+
+
+@app.route('/cursos')
+def listar_cursos():
+    cursos = Curso.query.all()
+    return render_template('cursos.html', cursos=cursos)
+
+
+@app.route('/novo_curso', methods=['GET', 'POST'])
+def novo_curso():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        curso = Curso(nome=nome)
+        db.session.add(curso)
+        db.session.commit()
+        return redirect(url_for('listar_cursos'))
+    return render_template('novo_curso.html')
+
+
+@app.route('/nova_matricula', methods=['GET', 'POST'])
+def nova_matricula():
+    alunos = Aluno.query.all()
+    cursos = Curso.query.all()
+    if request.method == 'POST':
+        aluno_id = request.form['aluno_id']
+        curso_id = request.form['curso_id']
+        matricula = Matricula(aluno_id=aluno_id, curso_id=curso_id)
+        db.session.add(matricula)
+        db.session.commit()
+        return redirect(url_for('relatorio'))
+    return render_template('nova_matricula.html', alunos=alunos, cursos=cursos)
 
 
 if __name__ == '__main__':
